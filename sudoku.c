8a145d50c3a6081273ef16846614a04c585e63f7
SaymonBonifacio <saymonbonifacio@gmail.com>
2019-06-06 18:59:27 -0300

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -5,13 +5,18 @@ int main() {
 	int opcao;
 	printf("\nBem-vindo.\n\n");
 	printf("O que deseja fazer?\n\n");
-	printf("1 - Novo jogo.\n");
-	printf("2 - Carregar um jogo.\n\n");
+	printf("Menu:\n");
+	printf("1 - Novo jogo\n");
+	printf("2 - Carregar um jogo salvo\n");
+	printf("3 - Excluir um jogo salvo\n");
+	printf("4 - Sair\n\n");
 	printf("Selecione uma das opcoes acima: ");
 	scanf("%d", &opcao);
 	switch (opcao) {
-		case 1: printf("Novo jogo.\n"); break;
-		case 2: printf("Carregar um jogo.\n"); break;
+		case 1: novo_jogo(); break;
+		case 2: carregar_jogor(); break;
+		case 3: excluir_jogo(); break;
+		case 4: return 0;
 		default: printf("Opcao invalida.\n"); break;
 	}
 	return 0;

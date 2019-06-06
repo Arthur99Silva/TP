29dff9e327cd90bdeea83a8dc1f0844ccc3fa698
SaymonBonifacio <saymonbonifacio@gmail.com>
2019-06-06 20:04:08 -0300

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 
 int novo_jogo() {
-	
+	printf("\n");
+	printf("     1  2  3     4  5  6     7  8  9\n");
+	printf("  +-----------+-----------+----------+\n");
+	for (int i = 0; i < 9; i++) {
+		printf("%c |", (65+i));
+		for (int j = 0; j < 9; j++) {
+			printf(" %2d", j+1);
+			if (j == 2 || j == 5) {
+				printf("  |");
+			}
+		}
+		printf(" |\n");
+		if (i == 2 || i == 5) {
+			printf("  +-----------+-----------+----------+\n");
+		}
+	}
+	printf("  +-----------+-----------+----------+\n\n");
 }
 
 int main() {
@@ -18,8 +34,8 @@ int main() {
 	scanf("%d", &opcao);
 	switch (opcao) {
 		case 1: novo_jogo(); break;
-		case 2: carregar_jogor(); break;
-		case 3: excluir_jogo(); break;
+		/*case 2: carregar_jogor(); break;
+		case 3: excluir_jogo(); break;*/
 		case 4: return 0;
 		default: printf("Opcao invalida.\n"); break;
 	}
